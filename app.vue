f47ccd0bcf4489010e8f5e9b7deb6f8a1f9512a8
Maira Hernandez <maira31821956@gmail.com>
2023-10-30 05:17:54 -0400

    feat: Crear componente para mostrar usuarios

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Usuarios />
     <NuxtWelcome />
   </div>
 </template>
---
+++ components/Usuarios.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1>Users</h1>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.name }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { PrismaClient } from '@prisma/client';
+  
+  const prisma = new PrismaClient();
+  
+  export default {
+    name: 'Users',
+    async fetch() {
+      const users = await prisma.usuario.findMany();
+  
+      return { users };
+    },
+  };
+  </script>
