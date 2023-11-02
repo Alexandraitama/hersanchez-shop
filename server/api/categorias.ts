ecd2fc57bd862a2fffce718538bcefdfd5b63b60
Maira Hernandez <maira31821956@gmail.com>
2023-11-02 18:26:59 -0400

    refactor: Remover imagenes sin usar

--- server/api/categorias.ts
+++ server/api/categorias.ts
@@ -1,21 +1,20 @@
 // api/categorias.js
 
 import { defineEventHandler } from 'h3';
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
-    try{
+  try {
 
-        const categorias = await prisma.categoria.findMany();
-        
-        console.log('cat here: ', categorias)
-        return {
-            categorias
-        };
-        
-    } catch(error){
-        console.error(error)
-    }
+    const categorias = await prisma.categoria.findMany();
+
+    return {
+      categorias
+    };
+
+  } catch (error) {
+    console.error(error)
+  }
 });
