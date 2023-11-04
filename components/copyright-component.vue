104d7b8796c9571b8cdc2b744853c3f348fc3768
Maira Hernandez <maira31821956@gmail.com>
2023-11-04 16:35:25 -0400

    ajustar copyright

--- components/copyright-component.vue
+++ components/copyright-component.vue
@@ -3,10 +3,10 @@
 <template>
 <div class="bg-gray-800 py-4">
         <div class="container flex items-center justify-between">
-            <p class="text-white">&copy; TailCommerce - All Right Reserved</p>
-            <div>
+            <p class="text-white">&copy; hersanchez - Derechos Reservados</p>
+            <!-- <div>
                 <img src="/images/methods.png" alt="methods" class="h-5">
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
