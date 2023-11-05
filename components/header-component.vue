0c097f7afdf644b59d25a7de1ba8f43df7c6ce91
Maira Hernandez <maira31821956@gmail.com>
2023-11-05 11:05:44 -0400

    feat: Activar enlace a carrito

--- components/header-component.vue
+++ components/header-component.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="py-4 shadow-sm bg-white">
+  <header class="py-4 shadow-sm bg-white">
     <div class="container flex items-center justify-between">
       <a href="index.html">
         <img src="/images/logo.svg" alt="Logo" class="w-32">
@@ -11,21 +11,21 @@
         </span>
         <input type="text" name="search" id="search"
           class="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex"
-             placeholder="Buscar">
+          placeholder="Buscar">
         <button
           class="bg-primary border border-primary text-white px-8 py-3 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex">Buscar</button>
       </div>
 
-    <div class="flex items-center space-x-4">
-        <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
+      <div class="flex items-center space-x-4">
+        <nuxt-link to="/carrito" class="text-center text-gray-700 hover:text-primary transition relative">
           <div class="text-2xl">
             <font-awesome-icon icon="fas fa-bag-shopping" />
           </div>
           <div class="text-xs leading-3">Carrito</div>
-          <div
+          <!-- <div
             class="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
-            2</div>
-        </a>
+            2</div> -->
+        </nuxt-link>
         <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
           <div class="text-2xl">
             <font-awesome-icon icon="fas fa-user" />
